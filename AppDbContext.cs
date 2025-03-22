3045998fd22adf565e39900f7691d3e4fd1b9c32
Марк Юрьевич <krmark46@gmail.com>
2025-03-22 20:45:11 +0200

    Create AppDbContext.cs

---
+++ AppDbContext.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DZ_4
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=BaseINDz;Integrated Security=True;TrustServerCertificate=True;");
+        }
+
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+
+
+            // User = Order
+            modelBuilder.Entity<Order>()
+                .Property(o => o.OrderDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            // Category = Product
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+            // Order = Product
+            modelBuilder.Entity<Order>()
+                .HasMany(o => o.Products)
+                .WithMany(p => p.Orders)
+                .UsingEntity(j => j.ToTable("OrderProducts"));
+
+
+            modelBuilder.Entity<UserOrderView>().ToView("UserOrders").HasNoKey();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
